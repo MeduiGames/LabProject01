df53f5918d88f6148f5903f6da005400fe150059
MeduiIthron <50959438+MeduiIthron@users.noreply.github.com>
2019-09-14 09:54:59 +0300

    .

---
+++ LabProject01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LabProject01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+            Console.Read();
+        }
+    }
+}
